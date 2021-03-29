6ef31bd7f8b439678d6f307c2c295fd7e8404cde
drTricked <markdavid1009@hotmail.co.uk>
2021-03-29 13:54:34 -0400

    basic hangman game

---
+++ hangman/README.md
@@ -0,0 +1,25 @@
+# hangman
+
+Basic hangman game. Has a svg showing the hangman. Uses v-for, v-if, v-show. Components separated by header, figure, word, wrong letters. Currently just has a static set of words preloaded.
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ hangman/src/components/WrongLetters.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrong-letters-container">
+    <div class="wrong-letters">
+      <p>Wrong Letters</p>
+      <span v-for="(letter, index) in wrongLetters" :key="index">
+        {{ letter }}
+        <!-- if the current letter is not the last letter then add a comma after it -->
+        {{ index === wrongLetters.length - 1 ? "" : ", " }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      wrongLetters: { type: Array, default: () => [] },
+    },
+    watch: {
+      wrongLetters() {
+        if (this.wrongLetters.length === 6) {
+          this.$emit("endGame");
+        }
+      },
+    },
+    emits: ["endGame"],
+  };
+</script>
+
+<style></style>
